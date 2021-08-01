b4f944e4ea7b7e9a5a026dda3939e9dad0f8b71f
ulvimemmeedov <onezerolord@gmail.com>
2021-08-01 21:19:54 +0400

    1.0.5

--- src/Controllers/mongoBaseController.js
+++ src/Controllers/mongoBaseController.js
@@ -8,11 +8,52 @@ class MongoController {
 
         this._model = model;
         this.create = this.create;
-    
-    }
-    
-    Create = (req, res, next)=>{
 
+    }
+
+    Create = (req, res, next) => {
+
+        let obj = req.body;
+
+        if (!obj) {
+
+            res.json({
+
+                success: false,
+                msg: "request body undefined"
+
+            })
+
+        } else {
+
+            let object = new this._model(obj);
+
+            if (object) {
+
+                object.save()
+
+                return res.status(200).json({
+
+                    success: true,
+                    object: object
+
+                });
+
+
+            } else {
+
+                return res.status(500).json({
+
+                    success: false,
+                    msg: "Server Error"
+
+                })
+
+            }
+        }
+    }
+
+    Update = async (req, res, next) => {
         let obj = req.body;
 
         if (!obj) {
